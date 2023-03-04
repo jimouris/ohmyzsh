c8937509ed0fea92b06957778c944428770ea955
Dimitris Mouris <jimouris@gmail.com>
2023-03-04 14:43:03 -0500

    Minor updates

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -4,6 +4,7 @@ alias update='sudo apt update'
 alias upgrade='sudo apt upgrade'
 alias ohmyzsh="code ~/.oh-my-zsh"
 alias svi='sudo vim'
+alias cat='batcat'
 alias gitlg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 # anagrams
