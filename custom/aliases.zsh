f4a653068fcbe6ebeb2f3754b9aa8081290bad60
Dimitris Mouris <jimouris@udel.edu>
2023-05-18 17:37:34 -0400

    Minor updates

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -5,7 +5,11 @@ alias upgrade='sudo apt upgrade'
 alias ohmyzsh="code ~/.oh-my-zsh"
 alias svi='sudo vim'
 alias cat='batcat'
+alias top='bashtop'
+alias htop='bashtop'
+alias gc='git checkout'
 alias gitlg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias forcepull='git fetch origin && git reset --hard origin/main'
 
 # anagrams
 alias maek='make'
@@ -13,5 +17,3 @@ alias amke='make'
 alias mkae='make'
 alias reste='reset'
 alias reest='reset'
-
-# alias cat="bat"
--- custom/paths.zsh
+++ custom/paths.zsh
@@ -3,3 +3,10 @@ PATH="$HOME/bin:$PATH"
 
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 export PATH="$PATH/$JAVA_HOME/bin"
+
+source "$HOME/.cargo/env"
+
+GOROOT=/usr/local/go
+GOPATH=~/.go
+PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
