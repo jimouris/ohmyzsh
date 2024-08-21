2ac23ba85f170ff0a3b259b1bf384e5c5d75467b
Dimitris Mouris <dimitris@nillion.com>
2024-08-21 12:13:56 -0400

    minor additions

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -4,9 +4,9 @@ alias update='sudo apt update'
 alias upgrade='sudo apt upgrade'
 alias ohmyzsh="code ~/.oh-my-zsh"
 alias svi='sudo vim'
-alias cat='batcat'
-alias top='bashtop'
-alias htop='bashtop'
+alias cat='bat'
+alias top='htop'
+alias htop='htop'
 alias gc='git checkout'
 alias gitlg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias forcepull='git fetch origin && git reset --hard origin/main'
@@ -17,3 +17,5 @@ alias amke='make'
 alias mkae='make'
 alias reste='reset'
 alias reest='reset'
+
+alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'
--- custom/paths.zsh
+++ custom/paths.zsh
@@ -1,7 +1,11 @@
 # This is where you put your hand rolled scripts (remember to chmod them)
 PATH="$HOME/bin:$PATH"
 
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+if [ "$(uname)" == "Darwin" ]; then
+    export JAVA_HOME=$(/usr/libexec/java_home)
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+fi
 export PATH="$PATH/$JAVA_HOME/bin"
 export PAGER="most"
 
@@ -10,4 +14,23 @@ source "$HOME/.cargo/env"
 GOROOT=/usr/local/go
 GOPATH=~/.go
 PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+export PATH=/opt/homebrew/opt/python@3.11/libexec/bin:$PATH
+
+GPG_TTY=$(tty)
+export GPG_TTY
+
+export FOUNDRY_PATH="$HOME/.foundry/bin"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="${PYENV_ROOT}/bin:${PATH}"
+export J2_RENDER_VERSION=v0.1.0
+
+export NILLION_HOME="/Users/jimouris/repos/nillion/"
+
+function run-nada-tests() {
+  cd $NILLION_HOME && cargo test -p execution-engine-vm -p jit-compiler -p bytecode-evaluator -p nada-compiler-backend && cd -
+}
+
+PATH=$PATH:$HOME/.nilup/bin
+
+eval "$(fzf --zsh)"
 
